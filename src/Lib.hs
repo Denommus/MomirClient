4a972380d8715dfed846037e2d7b9e0572343099
Yuri Albuquerque <yuridenommus@gmail.com>
2016-06-29 16:50:52 -0300

    Forbidding negative numbers

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,9 +11,13 @@ searchButton cmc = do
   disabledAttr <- mapDyn disabled cmc
   (button', _) <- elDynAttr' "button" disabledAttr $ text "Search"
   let clickEvent = domEvent Click button'
-  return $ push return $ tagDyn cmc clickEvent
-    where disabled (Just _) = Map.empty
-          disabled Nothing  = Map.fromList [("disabled", "")]
+  return $ push filterNumber $ tagDyn cmc clickEvent
+    where disabled (Just x)
+            | x >= 0 = Map.empty
+          disabled _  = Map.fromList [("disabled", "")]
+          filterNumber (Just x)
+            | x >= 0 = return $ Just x
+          filterNumber _ = return Nothing
 
 cmcWidget :: (MonadWidget t m) => m (Dynamic t (Maybe Integer))
 cmcWidget = do
